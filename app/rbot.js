92154c39f9fb42f6825986e7242f75693cfec9b0
RUI <115133652+RuisanA@users.noreply.github.com>
2025-07-25 18:45:46 +0900

    Update rbot.js

--- app/rbot.js
+++ app/rbot.js
@@ -54,6 +54,10 @@ app.get("/", (req, res) => {
   res.send("Server is awake");
 });
 
+app.listen(3000, () => {
+    console.log(`App listening at http://localhost:${3000}`);
+});
+
 const admin_list = ["983317416283086909"]
 
 const client = new Client({
